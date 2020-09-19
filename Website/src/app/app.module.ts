abdb72681b3b2e9a0ddb91b61de2c5921afb4fdb
bathong188 <lebathong188@github.com>
2020-09-19 14:06:50 -0700

    added item cards

--- Website/src/app/app.module.ts
+++ Website/src/app/app.module.ts
@@ -10,8 +10,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { FormsModule } from '@angular/forms';
+import { BulbComponent } from './home/components/bulb/bulb.component';
+import { TabbarComponent } from './home/components/tabbar/tabbar.component';
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
+  declarations: [AppComponent, HomeComponent, BulbComponent, TabbarComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, FormsModule],
   providers: [
---
+++ Website/src/app/home/components/bulb/bulb.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bulb',
+  templateUrl: './bulb.component.html',
+  styleUrls: ['./bulb.component.css']
+})
+export class BulbComponent implements OnInit {
+  bulb:boolean = true;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
